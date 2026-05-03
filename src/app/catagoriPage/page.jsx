e948762cb796e95867f8788d1dcee28ba33ac800
first--github-project <amd142457@gmail.com>
2026-05-03 20:33:24 +0300

    fix: remove localhost fetch

--- src/app/catagoriPage/page.jsx
+++ src/app/catagoriPage/page.jsx
@@ -1,10 +1,6 @@
 import FilterPage from "@/componants/filterpage/FilterPage";
+import photos from "../../data/data.json";
 
-export default async function Page() {
-  const res = await fetch("http://localhost:3000/data.json", {
-    cache: "no-store",
-  });
-  const photos = await res.json();
-
+export default function Page() {
   return <FilterPage photos={photos}></FilterPage>;
 }
