a3e972066d0169f7d6c35b9a402173097137cd42
first--github-project <amd142457@gmail.com>
2026-05-03 20:44:24 +0300

    fix: auth config

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,13 +3,13 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("online-store");
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
+  database: mongodbAdapter(client.db("online-store"), {
     client,
   }),
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
   emailAndPassword: {
     enabled: true,
   },
